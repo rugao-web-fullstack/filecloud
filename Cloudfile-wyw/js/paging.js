e6c5993afec648f428b1a6d3c07c6b5801f1a9b2
O-Mr-Wei-O <1223157723@qq.com>
2017-11-13 15:42:33 +0800

    beta

--- Cloudfile-wyw/js/paging.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $(".photo button:eq(0)").on("click",function () {
-        $("#smImg div:eq(0)").slideDown().siblings().slideUp();
-    })
-    $(".photo button:eq(1)").on("click",function () {
-        $("#smImg div:eq(1)").slideDown().siblings().slideUp();
-    })
-})
